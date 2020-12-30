ca15b00c44dd2fd644d5326c99c10f8e2f07c706
Hrvoje Šimić <shime@twobucks.co>
2020-12-30 15:53:28 +0100

    Add email and address to the ad form

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -2,6 +2,14 @@ class Ad < ApplicationRecord
   validates :city, presence: true
   validates :description, presence: true
   validates :kind, presence: true
-  validates :phone, presence: true
-  validates :consent, presence: true
+  validates :consent, presence: { message: "mora biti odobrena" }
+  validates :address, presence: { message: "ne smije biti prazna" }, on: [:create, :update]
+  validates :email, format: /@/, if: -> { email.present? }
+
+  validate :phone_or_email_present, on: [:create, :update]
+  def phone_or_email_present
+    if phone.blank? && email.blank?
+      errors.add(:base, "Email ili telefon su obavezni")
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_29_233911) do
+ActiveRecord::Schema.define(version: 2020_12_30_143638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_12_29_233911) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "consent"
+    t.string "address"
   end
 
 end
