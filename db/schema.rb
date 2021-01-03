aadefd90598fb7c5148310216937a516e65c88fa
Viktor <viktor.fonic@gmail.com>
2021-01-04 00:53:43 +0100

    Add missing index on Ad.kind; Fix db:seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_03_163430) do
+ActiveRecord::Schema.define(version: 2021_01_03_235304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
