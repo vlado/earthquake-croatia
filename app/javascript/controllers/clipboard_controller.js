96aa0c16890c218e282eb3a6fea7c27d6ac0d837
Maxime Laforet <me@macx.im>
2021-01-04 22:14:10 +0000

    Make linter happy

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -10,11 +10,11 @@ export default class extends Controller {
     clipboard.on("success", function() {
       toast({
         message: "<span class='icon mr-2'><svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M5 13l4 4L19 7' /></svg></span>Poveznica kopirana",
-        type: 'is-success',
-        position: 'top-center',
+        type: "is-success",
+        position: "top-center",
         closeOnClick: true,
         opacity: 0.9,
-      })
+      });
     });
   }
 
