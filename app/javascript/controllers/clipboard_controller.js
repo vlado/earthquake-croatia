4accf413b69157fece5fa84228e13240c517eb3a
Maxime Laforet <me@macx.im>
2021-01-04 22:05:19 +0000

    Replace toastr with bulma-toast

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "stimulus";
 import ClipboardJS from "clipboard";
-import toastr from "toastr";
+import { toast } from 'bulma-toast';
 
 export default class extends Controller {
   static targets = [ "button" ];
@@ -8,9 +8,13 @@ export default class extends Controller {
   connect() {
     var clipboard = new ClipboardJS(this.button);
     clipboard.on("success", function() {
-      toastr.success("Poveznica kopirana", "", {
-        closeButton: false, progressBar: false, timeOut: 2000, positionClass: "toast-top-center"
-      });
+      toast({
+        message: "<span class='icon mr-2'><svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M5 13l4 4L19 7' /></svg></span>Poveznica kopirana",
+        type: 'is-success',
+        position: 'top-center',
+        closeOnClick: true,
+        opacity: 0.9,
+      })
     });
   }
 
