09bb81593c99c1c6f07f8e4199c0e34b6ea36030
Vlado Cingel <vladocingel@gmail.com>
2021-01-05 16:07:29 +0100

    Add default from address to AdMailer

--- app/mailers/ad_mailer.rb
+++ app/mailers/ad_mailer.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class AdMailer < ApplicationMailer
+  default from: "do-not-reply@potres-petrinja.hr"
+
   def send_token(ad, action, url)
     @action = I18n.t("tokens.action.#{action}")
     @url = url
