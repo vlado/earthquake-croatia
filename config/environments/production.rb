8aa808cae1b0cb6c715ce12710eb8e2f268d1f3f
Vlado Cingel <vladocingel@gmail.com>
2021-01-05 15:20:48 +0100

    Mailgun config for sending emails on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,14 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'www.potres-petrinja.hr',
+    :authentication => :plain,
+  }
+  config.action_mailer.delivery_method = :smtp
 end
