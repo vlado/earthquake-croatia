e031dc117740aad492cd9fa093f9f71f9ec03583
Vlado Cingel <vladocingel@gmail.com>
2021-01-05 02:05:24 +0100

    Use doublequotes and semicolons in JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,9 +5,9 @@
 
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
-import $ from 'jquery'
-import 'select2'
-import 'select2/dist/css/select2.css'
+import $ from "jquery";
+import "select2";
+import "select2/dist/css/select2.css";
 
 // import * as ActiveStorage from "@rails/activestorage"
 // import "channels"
@@ -19,6 +19,6 @@ Turbolinks.start();
 import "controllers";
 
 document.addEventListener("turbolinks:load", function() {
-  $('.select2-container').remove() //remove the select2 container DOM
-  $('#city_id, #ad_city_id').select2();$ // Select2 will just reinit the DOM
+  $(".select2-container").remove(); //remove the select2 container DOM
+  $("#city_id, #ad_city_id").select2(); // Select2 will just reinit the DOM
 })
