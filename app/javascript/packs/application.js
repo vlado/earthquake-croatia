24f5b122f818f77c620f541db77ddfd6716d3dea
Viktor <viktor.fonic@gmail.com>
2021-01-06 12:19:42 +0100

    Make sure select2 uses HR language translations

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks";
 import $ from "jquery";
 import "select2";
 import "select2/dist/css/select2.css";
+import langhr from "select2/src/js/select2/i18n/hr";
 
 // import * as ActiveStorage from "@rails/activestorage"
 // import "channels"
@@ -19,9 +20,12 @@ Turbolinks.start();
 import "controllers";
 
 document.addEventListener("turbolinks:load", function() {
+  $.fn.select2.defaults.set("language", langhr);
+
   $(".select2-container").remove(); //remove the select2 container DOM
   $("#city_id").select2(); // Select2 will just reinit the DOM
   let $newAddCitySelect = $("#ad_city_id");
+
   $newAddCitySelect.select2({
     ajax: {
       url: $newAddCitySelect.data("url"),
