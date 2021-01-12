ce07b7a1440cdb565683b52a5ccfe88c0b9b159e
Vlado Cingel <vladocingel@gmail.com>
2021-01-12 16:20:20 +0100

    Additional SendGrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,13 +120,12 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => '465',
+    :port           => 587,
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_API_KEY'],
     :domain         => 'potres-petrinja.hr',
-    :enable_starttls_auto => true,
-    :ssl => true
+    :enable_starttls_auto => true
   }
   config.action_mailer.delivery_method = :smtp
 end
